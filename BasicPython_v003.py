b3512b603f84248e3ca3f8efe749258230e0dac1
davegoblue <dave@davegoblue.com>
2016-05-15 08:51:02 -0500

    Added XML portion of Chapter 13 UM Python

--- BasicPython_v003.py
+++ BasicPython_v003.py
@@ -180,3 +180,55 @@ url = raw_input("\nEnter - ")
 html = urllib.urlopen(url).read()
 links = re.findall('href="(http.*?://.*?)"', html)
 for link in links: print link
+
+
+
+# Chapter 13 - Using Web Services
+# Part 1 - XML
+# XML is a highly structured, expressive, and self-contained language
+# A tag is opened with <tagname> and closed with </tagname>
+# A tag may also have attributes -- <tagname type="abc"> stuff </tagname>
+# XML also has contracts between entities as to what is/is not valid XML for their specific exchange
+# XML can be parsed using ElementTree
+
+import xml.etree.ElementTree as ET
+
+print "\n***\t\t***\n"
+data = '''
+<person>
+    <name>Chuck</name>
+    <phone type="intl">
+        +1 734 303 4456
+    </phone>
+    <email hide="yes"/>
+</person>'''
+
+tree = ET.fromstring(data)
+print "Name:", tree.find("name").text
+print "Attr:", tree.find("email").get("hide")
+
+
+print "\n***\t\t***\n"
+input = '''
+<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall("users/user")
+print "User count:", len(lst)
+
+for item in lst:
+    print "Name", item.find("name").text
+    print "ID", item.find("id").text
+    print "Attribute", item.get("x")
+
